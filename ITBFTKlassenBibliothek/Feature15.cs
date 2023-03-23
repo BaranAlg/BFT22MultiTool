8d1de78aa3c9e6edc210f5db95b56d05e74c8abf
parsa.f3 <parsa.f3@P-0G-0B005-0389>
2023-03-23 08:20:11 +0100

    Branch erstellt.

---
+++ ITBFTKlassenBibliothek/Feature15.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITBFTKlassenBibliothek
+{
+    internal class Feature15
+    {
+        internal static void Feature_15()
+        {
+            Console.WriteLine("test");
+            Console.ReadKey();
+        }
+    }
+}
