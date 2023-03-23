2fa2267d857864fe92c2b6f53270f3525b95703d
Baran Algül <112069994+BaranAlg@users.noreply.github.com>
2023-03-23 10:30:36 +0100

    Update Feature17.cs

--- WIBFTKlassenBibliothek/Feature17.cs
+++ WIBFTKlassenBibliothek/Feature17.cs
@@ -1,1 +1,12 @@
 
+namespace MAKlassenBibliothek
+{
+   internal class Feature1
+   {
+       internal static void Feature_1()
+       {
+           Console.WriteLine("Test Feature 1"); 
+           //Diese Ausgabe hilft Ihnen zu erkennen ob der Aufruf funktioniert.
+       }
+   }
+}
