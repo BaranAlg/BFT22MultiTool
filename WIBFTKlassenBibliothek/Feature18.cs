e4b5086318e49aae4fe2e141b9e5bacc7f46c8ee
Ismael Zanati <ismael.z2@gso.schule.koeln>
2023-03-23 11:07:13 +0100

    immobilienrechner

--- WIBFTKlassenBibliothek/Feature18.cs
+++ WIBFTKlassenBibliothek/Feature18.cs
@@ -14,34 +14,35 @@ namespace WIBFTKlassenBibliothek
             Console.WriteLine("Willkommen beim Immobilienrechner");
 
             Console.WriteLine("Geben Sie den Kaufpreis der Immobilie ein: ");
-            double v = double.Parse(Console.ReadLine());
-            double kaufpreis = v;
+            double kaufpreis = double.Parse(Console.ReadLine());
+            double v = kaufpreis;
 
             Console.WriteLine("Geben Sie die Laufzeit des Darlehens in Jahren ein: ");
-            double v = double.Parse(Console.ReadLine());
+            double laufzeit = double.Parse(Console.ReadLine());
+            double l = laufzeit;
 
             Console.WriteLine("Geben Sie den jährlichen als Dezimalzahl ein: ");
             double zinssatz = double.Parse(Console.ReadLine());
+            double z = zinssatz;
 
-            {
 
 
-                // Eingabeaufforderungen für Kaltmiete und Nebenkosten
-                Console.WriteLine("Gib die Kaltmiete ein:");
-                double kaltmiete = double.Parse(Console.ReadLine());
+            // Eingabeaufforderungen für Kaltmiete und Nebenkosten
+            Console.WriteLine("Gib die Kaltmiete ein:");
+            double kaltmiete = double.Parse(Console.ReadLine());
 
-                Console.WriteLine("Gib die Nebenkosten ein:");
-                double nebenkosten = double.Parse(Console.ReadLine());
+            Console.WriteLine("Gib die Nebenkosten ein:");
+            double nebenkosten = double.Parse(Console.ReadLine());
 
-                // Berechnung der Warmmiete
-                double warmmiete = kaltmiete + nebenkosten;
+            // Berechnung der Warmmiete
+            double warmmiete = kaltmiete + nebenkosten;
 
-                // Ausgabe der Ergebnisse
-                Console.WriteLine($"Die Kaltmiete beträgt {kaltmiete:c}.");
-                Console.WriteLine($"Die Nebenkosten betragen {nebenkosten:c}.");
-                Console.WriteLine($"Die Warmmiete beträgt {warmmiete:c}.");
+            // Ausgabe der Ergebnisse
+            Console.WriteLine($"Die Kaltmiete beträgt {kaltmiete:c}.");
+            Console.WriteLine($"Die Nebenkosten betragen {nebenkosten:c}.");
+            Console.WriteLine($"Die Warmmiete beträgt {warmmiete:c}.");
 
-            }
+            
 
 
             double darlehensbetrag = kaufpreis;
