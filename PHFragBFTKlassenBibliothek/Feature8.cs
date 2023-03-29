6b361d1b6ecf74454a77098ae4d637dcff4dbd69
Khalid Edo Qasim Simoqy <khalidedoq.s@gso.schule.koeln>
2023-03-29 11:00:38 +0200

    Fertig mit dem Menü

--- PHFragBFTKlassenBibliothek/Feature8.cs
+++ PHFragBFTKlassenBibliothek/Feature8.cs
@@ -10,8 +10,56 @@ namespace PHFragBFTKlassenBibliothek
     {
         internal static void feature_8()
         {
-            Console.WriteLine("Alles Funktioniert!");
-            Console.ReadKey();
+           bool Exit = false;
+            
+           do
+           {
+                (int, int) cPosBM = Console.GetCursorPosition();
+
+                Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                                  "|                 >>> Infos zur Aufbau und Strucktur der Materie <<<               |\n" +
+                                  "------------------------------------------------------------------------------------\n");
+
+                Console.WriteLine("Eingabe: exit\t->\tbeendet das Programm");
+
+                Console.WriteLine("1. Atomphysik");
+                Console.WriteLine("2. Kernphysik");
+                Console.WriteLine("3. Relativitätstheorie");
+                Console.WriteLine("4. Astrophysik");
+                Console.Write("Wählen Sie bitte ein Punkt aus: ");
+                string menu = Console.ReadLine();
+
+                switch(menu)
+                {
+                    case "1":
+                        Console.WriteLine("Test");
+                        break;
+                    case "2":
+                        Console.WriteLine("Test");
+                        break;
+                    case "3":
+                        Console.WriteLine("Test");
+                        break;
+                    case "4":
+                        Console.WriteLine("Test");
+                        break;
+
+                    case "exit":
+                        Exit = true;
+                        break;
+                    default:
+
+                        Console.WriteLine("Ungültige Eingabe");
+                        Console.ReadKey();
+
+                        (int, int) cPosAM = Console.GetCursorPosition();
+
+                        KonsolenExtrasBibliothek.ConsoleExtras.ClearCurrentConsoleLine(cPosBM.Item2, cPosAM.Item2);
+
+                        break;
+                        Console.ReadKey();
+                }
+           } while (!Exit);
         }
     }
 }
