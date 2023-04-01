0932144d68205ddb2318d2f8b06d95f8e787e9af
Arthur Leon Dyga <arthurleon.d2@gso.schule.koeln>
2023-04-01 21:13:22 +0200

    Programm erweitert.

--- ITBFTKlassenBibliothek/Feature11.cs
+++ ITBFTKlassenBibliothek/Feature11.cs
@@ -1,6 +1,8 @@
 ﻿using Figgle;
 using System.Diagnostics;
 using System.Management;
+using System.Net;
+using System.Net.NetworkInformation;
 
 namespace ITBFTKlassenBibliothek
 {
@@ -91,9 +93,11 @@ namespace ITBFTKlassenBibliothek
                 }
                 static void Computer_Hardware_Ausgebene()
                 {
-                    Console.WriteLine("PC-Hardwareinformationen:");
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(FiggleFonts.Slant.Render("PC-Hardwareinformationen:"));
                     Console.WriteLine("-------------------------");
-
+                    Console.ForegroundColor = ConsoleColor.Green;
                     ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_Processor");
                     foreach (ManagementObject mo in searcher.Get())
                     {
@@ -121,7 +125,7 @@ namespace ITBFTKlassenBibliothek
                     searcher = new ManagementObjectSearcher("select * from Win32_DiskDrive");
                     foreach (ManagementObject mo in searcher.Get())
                     {
-                        Console.WriteLine("Disk Drive: {0}", mo["Caption"]);
+                        Console.WriteLine("Disk Drive: {0} ({1} GB)", mo["Caption"], Math.Round(Convert.ToDouble(mo["Size"]) / (1024 * 1024 * 1024), 2));
                     }
 
                     searcher = new ManagementObjectSearcher("select * from Win32_BaseBoard");
@@ -129,11 +133,44 @@ namespace ITBFTKlassenBibliothek
                     {
                         Console.WriteLine("Motherboard: {0}", mo["Product"]);
                     }
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Drücken Sie eine beliebige Taste zum Fortfahren.");
                     Console.ReadLine();
+                    Console.Clear();
                 }
                 static void PC_Auslastung()
                 {
-                    // PC_Auslastung
+                    Console.Clear();
+                    while (!Console.KeyAvailable)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(FiggleFonts.Slant.Render("Pc-Auslastung:"));
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        float cpuUsage = GetCpuUsage();
+                        float ramUsage = GetRamUsage();
+
+                        Console.WriteLine("\n\n\n\nAuslastung: " + cpuUsage.ToString("F") + "%");
+                        Console.WriteLine("RAM-Übrig: " + ramUsage.ToString("F") + " MB\n\n\n\n");
+
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Drücken Sie eine beliebige Taste zum Fortfahren.");
+                        System.Threading.Thread.Sleep(2500);
+                        Console.Clear();
+                    }
+                    static float GetCpuUsage()
+                    {
+                        PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+                        cpuCounter.NextValue();
+                        System.Threading.Thread.Sleep(2500);
+                        return cpuCounter.NextValue();
+                    }
+
+                    static float GetRamUsage()
+                    {
+                        PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+                        return ramCounter.NextValue();
+                    }
+
                 }
             } while (Schleife == true);
             Console.ForegroundColor = ConsoleColor.Green;
