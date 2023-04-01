86abc0e1fd43aec67eafa5dc0b5573c49c990190
Arthur Leon Dyga <arthurleon.d2@gso.schule.koeln>
2023-04-01 20:09:56 +0200

    Zwischenspeicherung

--- ITBFTKlassenBibliothek/Feature11.cs
+++ ITBFTKlassenBibliothek/Feature11.cs
@@ -1,6 +1,6 @@
 ﻿using Figgle;
 using System.Diagnostics;
-
+using System.Management;
 
 namespace ITBFTKlassenBibliothek
 {
@@ -55,21 +55,86 @@ namespace ITBFTKlassenBibliothek
 
                 static void PC_Prozesse()
                 {
-                    // PC_Prozesse
+                    Console.Clear();
+                    Process[] processes = Process.GetProcesses();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(FiggleFonts.Slant.Render("Laufende-Programme"));
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    for (int i = 0; i < processes.Length; i++)
+                    {
+                        if (!string.IsNullOrEmpty(processes[i].MainWindowTitle))
+                        {
+                            Console.WriteLine($"{i + 1}. {processes[i].MainWindowTitle}");
+                        }
+                    }
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Drücken Sie eine beliebige Taste zum Fortfahren.");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
                 static void Alle_HinterGrundProzesse()
                 {
-                    // Alle_HinterGrundProzesse
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(FiggleFonts.Slant.Render("Laufende Prozesse:"));
+                    Process[] processes = Process.GetProcesses();
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    foreach (Process process in processes)
+                    {
+                        Console.WriteLine(process.ProcessName);
+                    }
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Drücken Sie eine beliebige Taste zum Fortfahren.");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
                 static void Computer_Hardware_Ausgebene()
                 {
-                    // Alle_HinterGrundProzesse
+                    Console.WriteLine("PC-Hardwareinformationen:");
+                    Console.WriteLine("-------------------------");
+
+                    ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_Processor");
+                    foreach (ManagementObject mo in searcher.Get())
+                    {
+                        Console.WriteLine("CPU: {0}", mo["Name"]);
+                    }
+
+                    searcher = new ManagementObjectSearcher("select * from Win32_ComputerSystem");
+                    foreach (ManagementObject mo in searcher.Get())
+                    {
+                        Console.WriteLine("RAM: {0} GB", Math.Round(Convert.ToDouble(mo["TotalPhysicalMemory"]) / (1024 * 1024 * 1024), 2));
+                    }
+
+                    searcher = new ManagementObjectSearcher("select * from Win32_VideoController");
+                    foreach (ManagementObject mo in searcher.Get())
+                    {
+                        Console.WriteLine("GPU: {0}", mo["Name"]);
+                    }
+
+                    searcher = new ManagementObjectSearcher("select * from Win32_NetworkAdapter where NetConnectionStatus = 2");
+                    foreach (ManagementObject mo in searcher.Get())
+                    {
+                        Console.WriteLine("Network Adapter: {0}", mo["Name"]);
+                    }
+
+                    searcher = new ManagementObjectSearcher("select * from Win32_DiskDrive");
+                    foreach (ManagementObject mo in searcher.Get())
+                    {
+                        Console.WriteLine("Disk Drive: {0}", mo["Caption"]);
+                    }
+
+                    searcher = new ManagementObjectSearcher("select * from Win32_BaseBoard");
+                    foreach (ManagementObject mo in searcher.Get())
+                    {
+                        Console.WriteLine("Motherboard: {0}", mo["Product"]);
+                    }
+                    Console.ReadLine();
                 }
                 static void PC_Auslastung()
                 {
                     // PC_Auslastung
                 }
-                Console.ReadKey();
             } while (Schleife == true);
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Clear();
