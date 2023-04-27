548f97a37d4e1879e76e311a5f5d4583e2346678
Khalid Edo Qasim Simoqy <khalidedoq.s@gso.schule.koeln>
2023-04-27 08:02:13 +0200

    MultiTool

--- PHFragBFTKlassenBibliothek/Feature8.cs
+++ PHFragBFTKlassenBibliothek/Feature8.cs
@@ -32,16 +32,16 @@ namespace PHFragBFTKlassenBibliothek
                 switch(menu)
                 {
                     case "1":
-                        Console.WriteLine("Test");
+                        Console.WriteLine("\nAtomphysik. Die Atomphysik untersucht den Aufbau der Atome aus Atomkern und \nElektronenhülle und die Wechselwirkungen der Atome und Ionen mit anderen Atomen oder Ionen, mit Festkörpern, mit elektromagnetischer Strahlung und Teilchenstrahlung, mit elektrischen und magnetischen Feldern. Meist wird der Begriff auf die Untersuchung von Struktur ");
                         break;
                     case "2":
-                        Console.WriteLine("Test");
+                        Console.WriteLine("Die Kernphysik ist ein Teilbereich der Physik, der sich mit dem Aufbau und dem Verhalten von Atomkernen \nbeschäftigt. Während die Atomphysik sich mit der Physik der Atomhülle befasst, ist Gegenstand der Kernphysik die Aufklärung der Kernstruktur, also der Einzelheiten des Aufbaus der Atomkerne. Hierzu werden beispielsweise spontane Umwandlungen der Atomkerne untersucht");
                         break;
                     case "3":
-                        Console.WriteLine("Test");
+                        Console.WriteLine("Die Relativitätstheorie ist eine Theorie der Physik, die sich mit der Struktur von Raum und Zeit sowie mit \ndem Wesen der Gravitation befasst. Sie besteht aus zwei maßgeblich von Albert Einstein entwickelten physikalischen Theorien: der 1905 veröffentlichten speziellen Relativitätstheorie und der 1916 abgeschlossenen allgemeinen Relativitätstheorie1");
                         break;
                     case "4":
-                        Console.WriteLine("Test");
+                        Console.WriteLine("Die Astrophysik ist ein Teilgebiet der Astronomie, das sich mit der Untersuchung der physikalischen Beschaffenheit \nder kosmischen Objekte befasst. Sie nimmt gegenwärtig den breitesten Raum innerhalb der Astronomie ein1.\r\n\r\n");
                         break;
 
                     case "exit":
