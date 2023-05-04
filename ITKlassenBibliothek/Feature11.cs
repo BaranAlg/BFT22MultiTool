7da3bec169979110c2a6e9ce5a54e14802c0dd93
Kander Akinci <k.akinci@gso.schule.koeln>
2023-05-04 09:19:25 +0200

    UD

---
+++ ITKlassenBibliothek/Feature11.cs
@@ -0,0 +1,167 @@
+﻿using Figgle;
+using System.Diagnostics;
+using System.Management;
+
+namespace ITBFTKlassenBibliothek
+{
+    internal class Feature11
+    {
+        internal static void TaskManager()
+        {
+            bool Schleife = true;
+            bool Crash;
+            string sAuswahl;
+            int iAuswahl;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Clear();
+                Console.WriteLine(FiggleFonts.Slant.Render("Task-Manager"));
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Menüpunkte:");
+                Console.WriteLine("1. PC Prozesse");
+                Console.WriteLine("2. Alle HintergrundProzesse");
+                Console.WriteLine("3. Computer hardware ausgeben");
+                Console.WriteLine("4. PC Auslastung");
+                Console.WriteLine("5. Submenü verlassen");
+                Console.WriteLine("6. Exit");
+                sAuswahl = Console.ReadLine();
+                Crash = int.TryParse(sAuswahl, out iAuswahl);
+                if(Crash == false)
+                {
+                    Feature11.TaskManager();
+                }
+                switch (iAuswahl)
+                {
+                    case 1:
+                        PC_Prozesse();
+                        break;
+                    case 2:
+                        Alle_HinterGrundProzesse();
+                        break;
+                    case 3:
+                        Computer_Hardware_Ausgebene();
+                        break;
+                    case 4:
+                        PC_Auslastung();
+                        break;
+                    case 5:
+                        Schleife = false;
+                        break;
+                    case 6:
+                        Environment.Exit(0);
+                        break;
+                }
+
+                static void PC_Prozesse()
+                {
+                    Console.Clear();
+                    Process[] processes = Process.GetProcesses();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(FiggleFonts.Slant.Render("Laufende-Programme"));
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    foreach (var process in processes)
+                    {
+                        if (!string.IsNullOrEmpty(process.MainWindowTitle))
+                        {
+                            int index = Array.IndexOf(processes, process);
+                            Console.WriteLine($"{index + 1}. {process.MainWindowTitle}");
+                        }
+                    }
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Drücken Sie eine beliebige Taste zum Fortfahren.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                static void Alle_HinterGrundProzesse()
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(FiggleFonts.Slant.Render("Laufende Prozesse:"));
+                    Process[] processes = Process.GetProcesses();
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    foreach (Process process in processes)
+                    {
+                        Console.WriteLine(process.ProcessName);
+                    }
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Drücken Sie eine beliebige Taste zum Fortfahren.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                static void Computer_Hardware_Ausgebene()
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(FiggleFonts.Slant.Render("PC-Hardwareinformationen:"));
+                    Console.WriteLine("-------------------------");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_Processor");
+                    foreach (ManagementObject mo in searcher.Get())
+                    {
+                        Console.WriteLine("CPU: {0}", mo["Name"]);
+                    }
+
+                    searcher = new ManagementObjectSearcher("select * from Win32_ComputerSystem");
+                    foreach (ManagementObject mo in searcher.Get())
+                    {
+                        Console.WriteLine("RAM: {0} GB", Math.Round(Convert.ToDouble(mo["TotalPhysicalMemory"]) / (1024 * 1024 * 1024), 2));
+                    }
+
+                    searcher = new ManagementObjectSearcher("select * from Win32_VideoController");
+                    foreach (ManagementObject mo in searcher.Get())
+                    {
+                        Console.WriteLine("GPU: {0}", mo["Name"]);
+                    }
+
+                    searcher = new ManagementObjectSearcher("select * from Win32_NetworkAdapter where NetConnectionStatus = 2");
+                    foreach (ManagementObject mo in searcher.Get())
+                    {
+                        Console.WriteLine("Network Adapter: {0}", mo["Name"]);
+                    }
+
+                    searcher = new ManagementObjectSearcher("select * from Win32_DiskDrive");
+                    foreach (ManagementObject mo in searcher.Get())
+                    {
+                        Console.WriteLine("Disk Drive: {0} ({1} GB)", mo["Caption"], Math.Round(Convert.ToDouble(mo["Size"]) / (1024 * 1024 * 1024), 2));
+                    }
+
+                    searcher = new ManagementObjectSearcher("select * from Win32_BaseBoard");
+                    foreach (ManagementObject mo in searcher.Get())
+                    {
+                        Console.WriteLine("Motherboard: {0}", mo["Product"]);
+                    }
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Drücken Sie eine beliebige Taste zum Fortfahren.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                static void PC_Auslastung()
+                {
+                    Console.Clear();
+                    PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+                    PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+                    while (!Console.KeyAvailable)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(FiggleFonts.Slant.Render("Pc-Auslastung:"));
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        float cpuUsage = cpuCounter.NextValue();
+                        float ramUsage = ramCounter.NextValue();
+
+                        Console.WriteLine($"\n\n\n\nAuslastung: {cpuUsage:F2}%");
+                        Console.WriteLine($"RAM-Übrig: {ramUsage}MB\n\n\n\n");
+
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Drücken Sie eine beliebige Taste zum Fortfahren.");
+                        System.Threading.Thread.Sleep(2500);
+                        Console.Clear();
+                    }
+                }
+            } while (Schleife == true);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+        }
+    }
+}
---
+++ ITKlassenBibliothek/ITSubMenue.cs
@@ -0,0 +1,99 @@
+﻿using Figgle;
+
+
+
+namespace ITBFTKlassenBibliothek
+{
+    public class ITSubMenue
+    {
+        public static void ITSubMenue1()
+        {
+            bool Exit = false;
+
+            do
+            {
+
+                (int, int) cPosBM = Console.GetCursorPosition();
+
+                Console.WriteLine();
+
+                //ASCII art Logo wird erzeugt.
+                Console.WriteLine
+                    (FiggleFonts.Slant.Render("BFTMultiTool"));
+
+                //Konsolentitel wird geändert.
+                Console.Title = "BFTMultiTool";
+
+                Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                                  "                              >>> ItMenü <<<\n" +
+                                  "------------------------------------------------------------------------------------\n\n");
+
+                Console.WriteLine("Eingabe: exit\t->\tbeendet das Programm");
+
+                //Beschreibung der Software.
+                Console.WriteLine("\n\nMit der Software 'BFTMulti-Tool' sollen wiederkehrende oder besonders aufwendige\n" +
+                                  "Aufgabenaus dem schulischen Kontext erleichtert oder gelöst werden. Diese Aufgaben\n" +
+                                  "ergeben sich aus den Problemstellungen aus dem Unterricht der Berufsfachschule für Technik.\n\n");
+
+                string HauptAusw;
+
+
+                //Eingabeaufforderung 
+                Console.WriteLine("Wählen Sie eine der folgenden Themenbereiche:\n");
+                Console.WriteLine("\tb - Bits und Bytes umrechner");
+                Console.WriteLine("\tv - Video/Bild/Audio berechner");
+                Console.WriteLine("\tt - Task-Manager");
+                Console.WriteLine("\tp - Passender Arbeitsspeicher rechner");
+                Console.WriteLine("\td - Datei Pfad ausgeben\n");
+                Console.Write("Eingabe:");
+                HauptAusw = Console.ReadLine().ToLower();
+
+                switch (HauptAusw)
+                {
+                    case "b":
+                        Console.Clear();
+                        Feature15.Feature_15();
+                        break;
+
+                    case "v":
+                        Console.Clear();
+                        Feature12.Feature_12();
+                        break;
+
+                    case "t":
+                        Console.Clear();
+                        Feature11.TaskManager();
+                        break;
+
+                    case "p":
+                        Console.Clear();
+                        Feature14.Feature_14();
+                        break;
+
+                    case "d":
+                        Console.Clear();
+                        Feature13.Feature_13();
+                        break;
+
+                    case "exit":
+                        Exit = true;
+                        Console.Clear();
+                        break;
+                    default:
+
+                        Console.WriteLine("Ungültige Eingabe");
+                        Console.ReadKey();
+
+                        (int, int) cPosAM = Console.GetCursorPosition();
+
+                        KonsolenExtrasBibliothek.ConsoleExtras.ClearCurrentConsoleLine(cPosBM.Item2, cPosAM.Item2);
+
+                        break;
+                }
+
+            } while (!Exit);
+
+
+        }
+    }
+}
